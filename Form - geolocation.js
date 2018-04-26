43a51b02f45dbf58c72a82389542b057212ebf4b
pariwesh06 <pariweshg@gmail.com>
2018-04-26 15:20:51 +0530

    Add files via upload

---
+++ Form - geolocation.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import {
+  Platform, StyleSheet, Text, View, TextInput, Button, Picker,
+  FlatList
+} from 'react-native';
+const details_page = 'details';
+import { PermissionsAndroid } from 'react-native';
+
+function requestPermission() {
+  // alert('test');
+  try {
+    return PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        'title': 'Cool Photo App Camera Permission',
+        'message': 'allow geolocation?.'
+      }
+    )
+  } catch (err) {
+    console.warn(err)
+  }
+}
+
+
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      language: 'javascript',
+      user: 'ram'
+    };
+  }
+  getLocation() {
+    requestPermission().then((permission) => {
+      console.log(permission)
+      if (permission === PermissionsAndroid.RESULTS.GRANTED)
+        navigator.geolocation.getCurrentPosition((location) => console.log(location));
+    }, 
+    ()=>{console.log(arguments)})
+  }
+
+  search() {
+    var promise = fetch('http://it-ebooks-api.info/v1/search/angular');
+    promise.then((response) => {
+      response.json().then((data) => {
+        console.log(data.Books);
+        this.setState({ users: data.Books });
+      });
+    })
+    //logic
+  }
+  gotoDetails() {
+    // console.log(this.props.navigation);
+    this.props.navigation.navigate("details", { title: 'Javascript', price: 300 });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Userform, {this.state.language}
+        </Text>
+        <TextInput value={this.state.user} onChangeText={(text) => this.setState({ user: text })} />
+        <Button onPress={this.search.bind(this)} title="search" />
+        <Button onPress={this.gotoDetails.bind(this)} title="go to details" />
+        <Button onPress={this.getLocation.bind(this)} title="getLocation" />
+
+        <Picker
+          selectedValue={this.state.language}
+          style={{ height: 50, width: 300, backgroundColor: 'red' }}
+          onValueChange={(book, itemIndex) => {
+            this.props.navigation.navigate(details_page, book);
+            {/*this.setState({ language: itemValue })*/ }
+          }}>
+          {this.state.users.map(item => {
+            return <Picker.Item label={item.Title} value={item} />
+          })
+          }
+
+        </Picker>
+        <FlatList
+          data={this.state.users}
+          renderItem={(book) => <Text>{book.Title}</Text>}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
